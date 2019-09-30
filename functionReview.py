41cbbd198a5cc14f0ab47d631d0f26f952b3b863
Lopkit <sophiatanofficial@gmail.com>
2019-09-30 15:57:00 +0000

    first

---
+++ functionReview.py
@@ -0,0 +1,12 @@
+# def functionName(here, go, parameters):
+#     internalCode
+#     return returnValue
+
+#Function review on 7/18/2019, leap year
+def isLeapYear(year):
+    return (year%4 ==0)
+
+
+print(isLeapYear(1990))
+print(isLeapYear(1991))
+print(isLeapYear(1992))
